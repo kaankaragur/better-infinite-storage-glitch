84aa6d5e0e9e1e2085fe652117ff201e9212b482
karesi <109894399+kaankaragur@users.noreply.github.com>
2023-09-17 20:55:37 +0300

    Update setupBaseArray.py

--- setupBaseArray.py
+++ setupBaseArray.py
@@ -12,20 +12,33 @@ def findCopy(t,v):
     else:
        return False
     
-print("Assigning base64")
-base64_characters = list(string.ascii_uppercase + string.ascii_lowercase + string.digits + '+' + '/')
+print("[PROG] Assigning Base64 Characters")
+base64_characters = list(string.ascii_uppercase + string.ascii_lowercase + string.digits + '+' + '/' + '=')
 
-print("Assigning Colors")
+newBase64_2 = []
+print("[PROG][EXP] Assigning 2 Digits")
+#4096 is the all probilities. 64*64
+for char1 in base64_characters:
+    for char2 in base64_characters:
+        combination = char1+char2
+        newBase64_2.append(str(combination))
+
+
+
+newBase64 = newBase64_2 + base64_characters
+
+
+print("[PROG] Assigning Colors")
 colorValues = []
 similarityList = []
 lastestRandom = 0
-for i in range(len(base64_characters)):
+for i in range(len(newBase64)):
     goForBreak = False
     while True:
         if goForBreak == True:
             break
         randonRGB = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
-        if findCopy(colorValues,randonRGB) == True:
+        if findCopy(colorValues,randonRGB) == True or randonRGB == (0,0,0):
             continue
         else:
             for colorT in colorValues:
@@ -38,17 +51,19 @@ for i in range(len(base64_characters)):
                 if False:
                     break
                 else:
-                    print(f"[LOG] {str(randonRGB)} is saved out of {i}/63 with the similarity of {similarity}%")
+                    print(f"[LOG] {str(randonRGB)} is saved out of {i}/{len(newBase64)} with the similarity of {similarity}%", end='\r')
                     colorValues.append(randonRGB)
                     similarityList.append(similarity)
                     goForBreak = True
                     lastestRandom = similarity
                     break
             if len(colorValues) == 0:
-                print(f"[LOG] {str((255,255,255))} is saved out of {i}/63 with the similarity of 0%")
+                print(f"[LOG] {str((255,255,255))} is saved out of {i}/{len(newBase64)} with the similarity of 0%", end='\r')
                 colorValues.append(randonRGB)
                 goForBreak = True
                 break
+
+
 
 sumsa = 0
 for jj in similarityList:
@@ -62,11 +77,14 @@ data2 = {}
 colorValues1 = copy.copy(colorValues)
 colorValues2 = copy.copy(colorValues)
 
-for base64_character in base64_characters:
+print("[PROG] Writing Data...")
+
+
+for base64_character in newBase64:
     data1[base64_character] = [base64_character,colorValues1[0]]
     colorValues1.pop(0)
 
-    data2[str(colorValues2[0])] = [base64_character,colorValues2[0]]
+    data2[str((colorValues2[0]))] = [base64_character,(colorValues2[0])]
     colorValues2.pop(0)
 
 print("[SAV] Writing defaultAssignment.json")
